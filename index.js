c71451d9dcb1d6ff844d96dcd45079050f217631
Morse <tspears2@hawk.iit.edu>
2022-02-23 02:39:02 -0800

    Beginning cart webpage

---
+++ index.js
@@ -0,0 +1,23 @@
+// --> DEPENDENCIES <--
+const express = require('express');
+const path = require('path');
+
+// --> SERVER INITIALIZATION <--
+const college = express()
+
+// --> LOAD ROUTERS <--
+college.enable('trust proxy');
+college.set('port', (process.env.PORT || 5000))
+college.use(express.json())
+
+// --> HANDLE ENDPOINTS <--
+college.use('/assets', express.static(path.join(__dirname, 'assets')));
+college.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/cart/'))
+})
+
+
+// --> NOTIFY STARTUP <--
+college.listen(college.get('port'), () => {
+    console.log('Node app is running at localhost:' + college.get('port'))
+})
