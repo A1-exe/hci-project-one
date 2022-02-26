b96e915586007f600cd90aa0087a1eb3bbaced77
Morse <tspears2@hawk.iit.edu>
2022-02-26 15:57:28 -0800

    Add comments

--- index.js
+++ index.js
@@ -13,11 +13,12 @@ college.use(express.json())
 // --> HANDLE ENDPOINTS <--
 college.use('/assets', express.static(path.join(__dirname, 'assets')));
 college.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/cart/'))
+  // Serve cart as homepage (for now)
+  res.sendFile(path.join(__dirname, '/cart/'))
 })
 
 
 // --> NOTIFY STARTUP <--
 college.listen(college.get('port'), () => {
-    console.log('Node app is running at localhost:' + college.get('port'))
+  console.log('Node app is running at localhost:' + college.get('port'))
 })
